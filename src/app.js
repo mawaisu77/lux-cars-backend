1ef3c9feca8e03896868e07897e9c59dd3c45fc4
zaki-20 <zakibutt199@gmail.com>
2024-07-08 20:48:24 +0500

    cors middleware

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const morgan = require('morgan')
 const asyncHandler = require('express-async-handler');
 const ApiError = require('./utils/ApiError.js');
+const cors = require("cors")
 
 const app = express()
+
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(morgan('dev'));

1fc9639821e1921a90ffd01eb3c2310b052e7ae6
mawaisu77 <awaisullah.dev@gmail.com>
2024-07-19 00:20:04 +0500

    Changes in index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ const { app } = require('./app.js');
 
 dotenv.config({ path: `${process.cwd()}/.env` });
 
-const PORT =  8000;
+// const PORT =  8000;
 
-app.listen(PORT, () => {
-    console.log('Server up and running', PORT);
+// app.listen(PORT, () => {
+//     console.log('Server up and running', PORT);
+// });
+
+// Use PORT provided in environment or default to 3000
+const port = process.env.PORT || 3000;
+
+// Listen on `port` and 0.0.0.0
+app.listen(port, "0.0.0.0", function () {
+   console.log('Server up and running', port);
 });

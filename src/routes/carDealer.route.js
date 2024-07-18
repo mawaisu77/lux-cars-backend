857e3007354b5d04b40c153f6bde8c588b596b10
mawaisu77 <awaisullah.dev@gmail.com>
2024-07-18 22:40:45 +0500

    Changes in Upload LocalCar

--- src/routes/carDealer.route.js
+++ src/routes/carDealer.route.js
@@ -4,10 +4,6 @@ const { isAuthenticatedUser } = require("../middlewares/auth.js");
 const { upload } = require("../middlewares/multer.js");
 const router = Router()
 
-router.post('/localCars/uploadCar',  upload.fields([{
-                                            name: 'carImages', maxCount: 12
-                                        }]), uploadCar);
-
 
 router.post('/localCars/saveCarDealer',  upload.fields([{
                                             name: 'dealershipLicense', maxCount: 1

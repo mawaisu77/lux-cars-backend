47421acb193d16630b59bf346b9155abbf7e9048
shahryar <shahryarsheraz15@gmail.com>
2024-07-18 18:13:47 +0500

    db config changed

--- src/config/config.js
+++ src/config/config.js
@@ -20,10 +20,10 @@ module.exports =  {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_PROD_USERNAME,
+    "password": process.env.DB_PROD_PASSWORD,
+    "database": process.env.DB_PROD_NAME,
+    "host": process.env.DB_PROD_HOST,
+    "dialect": "postgres"
   }
 }

ee40b5a6a808f6c4c774a9e2123ec7bbe668dffc
shahryar <shahryarsheraz15@gmail.com>
2024-07-18 18:29:57 +0500

    some changes

--- src/config/config.js
+++ src/config/config.js
@@ -12,18 +12,13 @@ module.exports = {
     dialect: 'postgres',
     seederStorage: 'sequelize',
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
   "production": {
     username: process.env.DB_PROD_USERNAME,
     password: process.env.DB_PROD_PASSWORD,
     database: process.env.DB_PROD_NAME,
     host: process.env.DB_PROD_HOST,
-    dialect: "postgres"
+    port:process.env.DB_PROD_PORT,
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
   }
 }

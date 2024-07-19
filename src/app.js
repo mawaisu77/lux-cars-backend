828ddb72261fc50b3cc85931bd80a3da6f99184e
zaki-20 <zakibutt199@gmail.com>
2024-07-20 00:52:28 +0500

    update cors setting

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const corsOptions = {
   
   
 
-app.use(cors())
+app.use(cors(corsProd))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(morgan('dev'));

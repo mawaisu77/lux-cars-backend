ab6f048dc4cca22e235df96620e653d82d859605
mawaisu77 <awaisullah.dev@gmail.com>
2024-10-02 17:26:13 +0500

    Getting UnApproved Local Cars at Admin

--- src/routes/admin/adminLocalCars.route.js
+++ src/routes/admin/adminLocalCars.route.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const {
   changeCarStatus,
+  getAllUnApprovedLocalCars
 } = require("../../controllers/localCars.controller.js");
 const { isAuthenticatedAdmin } = require("../../middlewares/auth.js");
 const router = Router();
 
 router.post("/change-local-car-status", isAuthenticatedAdmin, changeCarStatus);
-
+router.get("/get-unapproved-local-cars", isAuthenticatedAdmin, getAllUnApprovedLocalCars)
 module.exports = router;

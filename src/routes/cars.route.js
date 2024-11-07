75391df001217b782ed18710c51e4558a222213d
mawaisu77 <awaisullah.dev@gmail.com>
2024-11-07 23:45:55 +0500

    Added APIS to get the History Cars and Calculate the Estimated Prices of the Cars

--- src/routes/cars.route.js
+++ src/routes/cars.route.js
@@ -1,5 +1,15 @@
 const { Router } = require("express");
-const { getAllCars, getCarByLotID, carsMakesModels, getAllCarsTesting, getCarByLotIDTesting, getHistoryCars, getALLCategoriesVehichleCount} = require("../controllers/cars.controller");
+const { 
+    getAllCars, 
+    getCarByLotID, 
+    carsMakesModels, 
+    getAllCarsTesting, 
+    getCarByLotIDTesting, 
+    getHistoryCars, 
+    getALLCategoriesVehichleCount, 
+    calculateEstimatedPriceForTheVehicle
+} = require("../controllers/cars.controller");
+
 const router = Router()
 
 router.get('/cars/get-all-cars', getAllCars)
@@ -9,5 +19,7 @@ router.get('/cars/get-car-by-lot-id/testing', getCarByLotIDTesting)
 router.get('/cars/get-cars-makes-models', carsMakesModels)
 router.get('/cars/get-history-cars', getHistoryCars)
 router.get('/cars/get-cars-counts', getALLCategoriesVehichleCount)
+router.get('/cars/get-cars-average-prices-data', calculateEstimatedPriceForTheVehicle)
+
 
 module.exports = router

6874104bfdb4840abbfdb68fcaa94b2b5f9da191
zaki-20 <zakibutt199@gmail.com>
2024-11-25 22:12:33 +0500

    add email template for registration

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -4,6 +4,8 @@ const nodeMailer = require("nodemailer");
 // const path = require('path');
 const emailConfig = require("./emailConfig.js");
 const logger = require("./logger.js");
+const fs = require('fs');
+const path = require('path');
 
 const createTransporter = () => {
     return nodeMailer.createTransport(emailConfig.smtp);
@@ -19,7 +21,24 @@ const sendEmail = async (options, contentType = 'html') => {
         };
         if (contentType === 'text') {
             mailOptions.text = options.message;
-        }    
+        } 
+        else if (contentType === 'html' && options.subject === 'LUX CAR Email Verification') {
+            const templatePath = path.join(__dirname, 'templates', 'lux-verification.html');
+            const htmlTemplate = fs.readFileSync(templatePath, 'utf-8');
+            const replacedHtml = htmlTemplate
+                .replace('{{username}}', options.username)
+                .replace('{{verificationUrl}}', options.verificationUrl)
+    
+            mailOptions.html = replacedHtml;
+               // Attach logo with Content-ID
+               mailOptions.attachments = [
+                {
+                    filename: 'logo.png',
+                    path: path.join(__dirname, '../../public/images/luxcar-logo.png'),
+                    cid: 'luxcars_logo',
+                },
+            ];
+        }
         await transporter.sendMail(mailOptions);
     } catch (error) {
         logger.error(`Error sending email: ${error}`)

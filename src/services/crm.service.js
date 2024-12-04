354ee7f1d8b6b49dbaac342122dffd55b80c8940
mawaisu77 <awaisullah.dev@gmail.com>
2024-12-04 23:49:44 +0500

    CRM Initial SetUP

--- src/services/crm.service.js
+++ src/services/crm.service.js
@@ -1,31 +1,56 @@
 const { axiosCRM } = require("../utils/axiosPrivate")
 
-const createCRMContact = async (req, res) => {
-    const sampleData = {
-        email: "john@deo.com",
-        phone: "+18887324197",
-        firstName: "John",
-        lastName: "Deo",
-        name: "John Deo",
-        dateOfBirth: "1990-09-25",
-        address1: "3535 1st St N",
-        city: "Dolomite",
-        state: "AL",
-        country: "US",
-        postalCode: "35061",
-        companyName: "DGS VolMAX",
-        website: "35061",
+const createCRMContact = async (user) => {
+
+    const body = {
+        email: user.email ,
+        phone: user.phone ? user.phone : "",
+        firstName: "",
+        lastName: "",
+        name: user.username,
+        dateOfBirth: "",
+        address1: user.address ? user.address : "",
+        city: "",
+        state: "",
+        country: "",
+        postalCode: "",
+        companyName: "",
+        website: "",
         tags: [
-            "aute consequat ad ea",
-            "dolor sed"
+            "Testing"
         ],
         source: "public api",
         customField: {
             "__custom_field_id__": "exercitation"
         }
     } 
+
+    const contact = await axiosCRM.post("/v1/contacts/", body)
+}
+
+const createCRMOpportunity = async (contact) => {
+
+    const body = {
+        "title": "Testing",
+        "status": "Testing",
+        "stageId": "62363159-9710-469f-a011-dbc589287bb7",
+        //"email": contact.email,
+        //"phone": contact.phone,
+        "assignedTo": "gZA5n2GA5cbLI82PQbEu",
+        "monetaryValue": 122.22,
+        //"source": contact.source,
+        //"contactId": contact.id,
+        // "name": contact.name,
+        // "companyName": contact.companyName,
+        // "tags": contact.tags
+    }
+    const pipelineId = ""
+    const opportunity = await axiosCRM.post(`/v1/pipelines/:${pipelineId}/opportunities/`, body)
+
+
 }
 
 module.exports = {
-    createCRMContact
+    createCRMContact,
+    createCRMOpportunity
 }

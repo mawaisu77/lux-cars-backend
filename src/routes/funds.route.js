bdf9120dcf64e5e1149104fb689b9a22ff939449
awaisi77 <captainawaisullah@gmail.com>
2025-03-04 22:17:18 +0500

    Updated add funds API to sync the adding funds process with successful payment

--- src/routes/funds.route.js
+++ src/routes/funds.route.js
@@ -1,10 +1,9 @@
 const { Router } = require("express");
 const { addFunds, getUserFunds } = require("../controllers/funds.controller.js");
 const { isAuthenticatedUser } = require("../middlewares/auth.js");
-const { processPayment } = require('../middlewares/payment.js')
 const router = Router()
 
-router.post('/funds/add-funds', isAuthenticatedUser, processPayment, addFunds)
+router.post('/funds/add-funds', isAuthenticatedUser, addFunds)
 router.get('/funds/get-funds', isAuthenticatedUser, getUserFunds)
 
 
